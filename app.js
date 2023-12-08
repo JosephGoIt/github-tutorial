91465f6406e84294ef783a452e5a2c8e1ed4c15d
Joseph Camacho <spongkj@gmail.com>
2023-12-08 23:53:58 +0800

    updated js

--- app.js
+++ app.js
@@ -15,3 +15,20 @@ switcher.addEventListener('click', function () {
     }
     console.log('current class name: ' + className);
 });
+
+        // Function to toggle the SVG href
+        function toggleSvgHref() {
+            // Get the <use> element
+            var useElement = document.getElementById('menuIcon');
+    
+            // Check the current href and toggle it
+            if (useElement.getAttributeNS('http://www.w3.org/1999/xlink', 'href') === '/icons.svg#gg_menu-left-icon') {
+                useElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/icons.svg#icon-x'); // Change to another icon
+            } else {
+                useElement.setAttributeNS('http://www.w3.org/1999/xlink', 'href', '/icons.svg#gg_menu-left-icon'); // Change back to the original icon
+            }
+        }
+    
+        // Add click event listener to the button
+        document.getElementById('toggleButton').addEventListener('click', toggleSvgHref);
+
